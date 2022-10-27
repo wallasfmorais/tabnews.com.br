f21579bfcb09138f675fa074a3e87135893d5db1
Ezequias <63757378+ezequiaslopesdasilva@users.noreply.github.com>
2022-10-27 18:40:00 +0000

    fix: remove Search Field Component from header

--- pages/interface/components/Header/index.js
+++ pages/interface/components/Header/index.js
@@ -1,10 +1,8 @@
 import { Header, Box, ActionMenu, ActionList, IconButton, Truncate, Text, Tooltip } from '@primer/react';
 import { PersonFillIcon, HomeIcon, SquareFillIcon } from '@primer/octicons-react';
 import { CgTab } from 'react-icons/cg';
-import { FiSearch } from 'react-icons/fi';
 import { useUser } from 'pages/interface/index.js';
 import { HeaderLink, Link } from 'pages/interface/components/Link';
-import { TextField } from '../TextField';
 import { useRouter } from 'next/router';
 
 export default function HeaderComponent() {
@@ -38,10 +36,6 @@ export default function HeaderComponent() {
         <HeaderLink href="/recentes" sx={pathname.startsWith('/recentes') && activeLinkStyle}>
           Recentes
         </HeaderLink>
-      </Header.Item>
-
-      <Header.Item>
-        <TextField placeholder="Pesquise conteúdos" trailingVisual={FiSearch} />
       </Header.Item>
 
       {!isLoading && !user && (
