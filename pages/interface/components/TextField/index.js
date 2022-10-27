c9931da72567ce736a4fea1635d2a82271395c20
Ezequias <63757378+ezequiaslopesdasilva@users.noreply.github.com>
2022-10-27 18:38:13 +0000

    fix(TextField): add Search field component

--- pages/interface/components/TextField/index.js
+++ pages/interface/components/TextField/index.js
@@ -1,4 +1,5 @@
-import { TextInput, TextInputProps } from '@primer/react';
+import { TextInput, TextInputProps, IconButton, ButtonGroup } from '@primer/react';
+import { FiSearch } from 'react-icons/fi';
 
 /**
  * @param {TextInputProps} param0
@@ -6,4 +7,28 @@ import { TextInput, TextInputProps } from '@primer/react';
  */
 export function TextField({ children, ...props }) {
   return <TextInput {...props} />;
+}
+
+export function SearchField({ children, ...props }) {
+  return (
+    <ButtonGroup
+      css={{
+        width: 'fill-available',
+        maxWidth: '75%',
+      }}>
+      <TextInput
+        css={{
+          width: 'fill-available',
+        }}
+        {...props}
+      />
+      <IconButton
+        aria-label="Search"
+        icon={FiSearch}
+        css={{
+          borderRadius: [0, , , 0],
+        }}
+      />
+    </ButtonGroup>
+  );
 }
